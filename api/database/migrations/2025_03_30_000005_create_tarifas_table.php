bc68ffed668a1790fc1292bd258b3185c426d8f0
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-03-31 01:01:07 +0200

    creación de las primeras tablas
    usuarios, socios, entrenadores, entrenamientos, tarifas y productos

---
+++ api/database/migrations/2025_03_30_000005_create_tarifas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tarifas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre')->unique();
+            $table->string('descripcion');
+            $table->integer('duracion'); //en meses
+            $table->decimal('precio', 5, 2); //999.99
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ofertas');
+    }
+};
