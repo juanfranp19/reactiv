2cf640d261ed8e7dcb496994c24b8af52f2e7cc7
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-06-05 20:07:27 +0200

    instalación bootstrap

--- acceso/src/main.js
+++ acceso/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
-import './style.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import App from './App.vue';
+import './style.css';
 
 createApp(App).mount('#app');
