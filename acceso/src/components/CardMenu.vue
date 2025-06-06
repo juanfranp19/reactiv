66574e72d2f667e8fdcbe4d40e458c4a0cba0e24
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-06-06 13:22:33 +0200

    menú de la página de inicio

---
+++ acceso/src/components/CardMenu.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="card-menu">
+        <div class="card-menu2">
+            <!-- contenido que se encuentra dentro de <CardMenu> -->
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/* https://uiverse.io/Tiagoadag/cuddly-catfish-6 */
+.card-menu {
+    width: 190px;
+    height: 254px;
+    background-image: linear-gradient(163deg, var(--verde) 0%, var(--azul) 100%);
+    border-radius: 20px;
+    transition: all .3s;
+}
+
+.card-menu2 {
+    width: 190px;
+    height: 254px;
+    background-color: var(--oscuro);
+    /* border-radius: 10px; */
+    transition: all .2s;
+
+    /* estilos para slot */
+    color: var(--verde);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card-menu2:hover {
+    transform: scale(0.98);
+    border-radius: 20px;
+}
+
+.card-menu:hover {
+    box-shadow: 0px 0px 30px 1px var(--verdeOscuro);
+}
+</style>
---
+++ acceso/src/components/Menu.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+import CardMenu from '@components/CardMenu.vue';
+
+</script>
+
+<template>
+    <nav class="row menu">
+
+        <div class="col-12 col-sm-6 menu-item">
+            <RouterLink to="/acceder">
+                <CardMenu>Acceder</CardMenu>
+            </RouterLink>
+        </div>
+
+        <div class="col-12 col-sm-6 menu-item">
+            <RouterLink to="/salir">
+                <CardMenu>Salir</CardMenu>
+            </RouterLink>
+        </div>
+
+    </nav>
+</template>
+
+<style scoped>
+.menu-item {
+    text-align: center;
+    padding: 5vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+/* estilos a partir de sm */
+@media screen and (min-width: 576px) {
+
+    .menu-item {
+        padding: 15vh;
+    }
+}
+
+/* estilos a partir de lg */
+@media screen and (min-width: 992px) {
+
+    .menu-item {
+        padding: 25vh;
+    }
+}
+</style>
