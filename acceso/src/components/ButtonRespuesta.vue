1894c8e1688cd3d9bfc9e93030950ad126646019
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-06-06 13:41:19 +0200

    card respuesta

---
+++ acceso/src/components/ButtonRespuesta.vue
@@ -0,0 +1,63 @@
+<script setup>
+
+defineProps({
+    titulo: String,
+});
+
+</script>
+
+<template>
+    <button class="button-respuesta">{{ titulo }}</button>
+</template>
+
+<style scoped>
+/* https://uiverse.io/cssbuttons-io/friendly-dragon-76 */
+.button-respuesta {
+    position: relative;
+    font-size: 17px;
+    text-transform: uppercase;
+    text-decoration: none;
+    padding: 1em 2.5em;
+    display: inline-block;
+    cursor: pointer;
+    border-radius: 6em;
+    transition: all 0.2s;
+    border: none;
+    font-family: inherit;
+    font-weight: 500;
+    color: black;
+    background-color: white;
+}
+
+.button-respuesta:hover {
+    transform: translateY(-3px);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+}
+
+.button-respuesta:active {
+    transform: translateY(-1px);
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+}
+
+.button-respuesta::after {
+    content: "";
+    display: inline-block;
+    height: 100%;
+    width: 100%;
+    border-radius: 100px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    transition: all 0.4s;
+}
+
+.button-respuesta::after {
+    background-color: #fff;
+}
+
+.button-respuesta:hover::after {
+    transform: scaleX(1.4) scaleY(1.6);
+    opacity: 0;
+}
+</style>
---
+++ acceso/src/components/CardRespuesta.vue
@@ -0,0 +1,82 @@
+<script setup>
+
+import ButtonRespuesta from '@components/ButtonRespuesta.vue';
+
+const WEB_URL = import.meta.env.VITE_WEB_URL;
+
+</script>
+
+<template>
+    <div class="row">
+        <div class="card-respuesta-flex">
+
+            <div class="card-respuesta">
+                <div class="row">
+
+                    <div class="col-12 titulo michroma-regular">
+                        <!-- contenido que se encuentra dentro de la etiqueta -->
+                        <slot></slot>
+                    </div>
+
+                    <!-- botón para volver al menú -->
+                    <div class="col-12">
+                        <RouterLink to="/">
+                            <ButtonRespuesta titulo="Volver al Menú" />
+                        </RouterLink>
+                    </div>
+
+                    <!-- botón para volver al dashboard -->
+                    <div class="col-12">
+                        <a :href="WEB_URL" target="_self">
+                            <ButtonRespuesta titulo="Volver al Dashboard" />
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card-respuesta-flex {
+    margin-top: 50px;
+    padding: 40px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card-respuesta {
+    width: 450px;
+    height: 350px;
+    border: none;
+    border-radius: 10px;
+
+    text-align: center;
+}
+
+.card-respuesta .titulo {
+    font-size: 30px;
+    padding: 50px 10px;
+    font-weight: bold;
+}
+
+.card-respuesta button {
+    margin: 15px 0;
+}
+
+/* estilos a partir de sm */
+@media screen and (min-width: 576px) {
+
+    .card-respuesta {
+        background-color: var(--verde);
+        box-shadow: 0px 0px 10px 1px var(--oscuro);
+    }
+
+    .card-respuesta .titulo {
+        font-weight: normal;
+    }
+}
+</style>
