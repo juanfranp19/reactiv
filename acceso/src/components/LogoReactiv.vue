1a4b38e538802ae905afc1ab1449b45930cc9d43
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-06-06 13:17:30 +0200

    topbar de acceso y .env

---
+++ acceso/src/components/LogoReactiv.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+const WEB_URL = import.meta.env.VITE_WEB_URL;
+
+</script>
+
+<template>
+    <a :href="WEB_URL">
+        <img class="logo-reactiv" src="..." alt="Logo Reactiv">
+    </a>
+</template>
---
+++ acceso/src/components/TopBar.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import LogoReactiv from '@components/LogoReactiv.vue';
+
+</script>
+
+<template>
+    <header class="row">
+        <div class="col-12 top-bar">
+            <LogoReactiv />
+        </div>
+    </header>
+</template>
+
+<style scoped>
+.top-bar {
+    background-color: var(--rojo);
+    text-align: center;
+    height: 100px;    
+}
+
+.top-bar .logo-reactiv {
+    width: 100px;
+    padding: 10px;
+}
+</style>
