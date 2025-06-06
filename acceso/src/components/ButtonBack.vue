74f07bbdaf3b87bcc1810347954fbc5576e6a5e4
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-06-06 13:33:43 +0200

    card de acceso

---
+++ acceso/src/components/ButtonBack.vue
@@ -0,0 +1,42 @@
+<template>
+    <RouterLink to="/">
+
+        <button class="button-back">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-rewind" viewBox="0 0 16 16">
+                <path d="M9.196 8 15 4.633v6.734zm-.792-.696a.802.802 0 0 0 0 1.392l6.363 3.692c.52.302 1.233-.043 1.233-.696V4.308c0-.653-.713-.998-1.233-.696z" />
+                <path d="M1.196 8 7 4.633v6.734zm-.792-.696a.802.802 0 0 0 0 1.392l6.363 3.692c.52.302 1.233-.043 1.233-.696V4.308c0-.653-.713-.998-1.233-.696z" />
+            </svg>
+            
+        </button>
+
+    </RouterLink>
+</template>
+
+<style scoped>
+/* https://uiverse.io/elijahgummer/sweet-rabbit-5 */
+.button-back {
+    font: inherit;
+    background-color: transparent;
+    border: 0;
+    color: var(--oscuro);
+    border-radius: 0.5em;
+    font-size: 1.35rem;
+    padding: 0.375em 1em;
+    font-weight: 600;
+    /* text-shadow: 0 0.0625em 0 #fff;
+    box-shadow: inset 0 0.0625em 0 0 #f4f4f4, 0 0.0625em 0 0 #efefef,
+      0 0.125em 0 0 #ececec, 0 0.25em 0 0 #e0e0e0, 0 0.3125em 0 0 #dedede,
+      0 0.375em 0 0 #dcdcdc, 0 0.425em 0 0 #cacaca, 0 0.425em 0.5em 0 #cecece; */
+    transition: 0.15s ease;
+    cursor: pointer;
+}
+
+.button-back:hover {
+    translate: 0 0.125em;
+}
+
+.button-back:active {
+    translate: 0 0.325em;
+}
+</style>
---
+++ acceso/src/components/CardAcceso.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+import ButtonBack from '@components/ButtonBack.vue';
+import FormAcceso from '@components/FormAcceso.vue';
+
+defineProps({
+    tipo: String, // acceso, salida
+});
+
+</script>
+
+<template>
+    <div class="row">
+        <div class="acceso-card-flex">
+
+            <div class="row acceso-card">
+                
+                <div class="col-12 titulo-card audiowide-regular" v-if="tipo === 'acceso'">
+                    Acceder
+                </div>
+
+                <div class="col-12 titulo-card audiowide-regular" v-if="tipo === 'salida'">
+                    Salir
+                </div>
+
+                <ButtonBack />
+                <FormAcceso :tipo="tipo" />
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.acceso-card-flex {
+    padding: 40px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.acceso-card {
+    text-align: center;
+    border-radius: 10px;
+    max-width: 500px;
+    padding-top: 90px;
+    opacity: 0.75;
+}
+
+.titulo-card {
+    font-size: 50px;
+}
+
+@media screen and (min-width: 576px) {
+
+    .acceso-card {
+        padding: 20px 50px 50px 50px;
+        background-color: var(--azul);
+    }
+}
+</style>
