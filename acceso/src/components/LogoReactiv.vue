d438b612810e193cfe91c76c5cd89402a9447ea8
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-06-15 22:29:44 +0200

    logo y título en index

--- acceso/src/components/LogoReactiv.vue
+++ acceso/src/components/LogoReactiv.vue
@@ -5,7 +5,9 @@ const WEB_URL = import.meta.env.VITE_WEB_URL;
 </script>
 
 <template>
+
     <a :href="WEB_URL">
-        <img class="logo-reactiv" src="..." alt="Logo Reactiv">
+        <img class="logo-reactiv" src="/logo-reactiv-rojo.svg" alt="Logo Reactiv">
     </a>
+    
 </template>
