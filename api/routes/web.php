04c1c649d09895b8e89c0a7dc936f6ceea01cee0
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-06-15 22:28:49 +0200

    ruta con información de la api

--- api/routes/web.php
+++ api/routes/web.php
@@ -4,9 +4,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return [
-        'App name' => env('APP_NAME', 'Laravel'),
+        'App name' => env('APP_NAME'),
+        'App license' => 'Reactiv © 2025 by Juan Francisco Pagán is licensed under CC BY-SA 4.0 https://creativecommons.org/licenses/by-sa/4.0/',
         'Laravel version' => app()->version(),
     ];
 });
 
-require __DIR__.'/storage.php';
+Route::get('/info-composer', function () {
+    $composer = json_decode(file_get_contents(base_path('composer.json')), true);
+    return response()->json($composer);
+});
+
+require __DIR__ . '/storage.php';
